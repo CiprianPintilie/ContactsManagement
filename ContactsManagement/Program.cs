657e62b5fe3bebf7fc06abad826c73224b087a45
Ciprian Pintilie <pintilie.c.a@gmail.com>
2019-05-29 09:50:42 +0200

    New - Initial solution

---
+++ ContactsManagement/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+
+namespace ContactsManagement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                config.SetBasePath(Directory.GetCurrentDirectory());
+                config.AddJsonFile("appsettings.json", true, true);
+            })
+            .ConfigureLogging(builder =>
+            {
+                builder.ClearProviders();
+                builder.SetMinimumLevel(LogLevel.Information);
+            })
+            .UseNLog()
+            .UseStartup<Startup>();
+    }
+}
