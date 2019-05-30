b32d2214588b011421a8fdfc20074b554d9e4fa2
Ciprian Pintilie <pintilie.c.a@gmail.com>
2019-05-30 18:28:55 +0200

    New - CompanyService unit tests

---
+++ UnitTests/BusinessLayer.UnitTests/CompanyServiceUnitTests.cs
@@ -0,0 +1,68 @@
+﻿using BusinessLayer.Services;
+using DataAccessLayer.Entities;
+using DataAccessLayer.Interop;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace BusinessLayer.UnitTests
+{
+    [TestClass]
+    public class CompanyServiceUnitTests
+    {
+        [TestMethod]
+        public void CompanyDataConflictsAsync_ShouldReturnTrue_IfACompanyAlreadyExistsWithTheProvidedVatAndName()
+        {
+            //Arrange
+
+            const string EXISTING_NAME = "Some NAME";
+            const string EXISTING_VAT = "Some VAT";
+
+            var companyCommandMock = new Mock<ICompanyCommand>();
+            var companyQueryMock = new Mock<ICompanyQuery>();
+
+            companyQueryMock
+                .Setup(i => i.GetByNameAsync(EXISTING_NAME))
+                .ReturnsAsync(new CompanyEntity());
+            companyQueryMock
+                .Setup(i => i.GetByVatAsync(EXISTING_VAT))
+                .ReturnsAsync(new CompanyEntity());
+
+            //Act
+
+            var companyService = new CompanyService(companyCommandMock.Object, companyQueryMock.Object);
+            var result = companyService.CompanyDataConflictsAsync(EXISTING_NAME, EXISTING_VAT).Result;
+
+            //Assert
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CompanyDataConflictsAsync_ShouldReturnFalse_IfNoCompanyExistsWithProvidedVatOrName()
+        {
+            //Arrange
+
+            const string EXISTING_NAME = "Some NAME";
+            const string EXISTING_VAT = "Some VAT";
+
+            var companyCommandMock = new Mock<ICompanyCommand>();
+            var companyQueryMock = new Mock<ICompanyQuery>();
+
+            companyQueryMock
+                .Setup(i => i.GetByNameAsync(EXISTING_NAME))
+                .ReturnsAsync((CompanyEntity)null);
+            companyQueryMock
+                .Setup(i => i.GetByVatAsync(EXISTING_VAT))
+                .ReturnsAsync((CompanyEntity)null);
+
+            //Act
+
+            var companyService = new CompanyService(companyCommandMock.Object, companyQueryMock.Object);
+            var result = companyService.CompanyDataConflictsAsync(EXISTING_NAME, EXISTING_VAT).Result;
+
+            //Assert
+
+            Assert.IsFalse(result);
+        }
+    }
+}
